2a06981bbe369b3f1a0a6512c6a557abd095eb96
onion <a0966550619@gmail.com>
2024-10-28 13:43:26 +0800

    讓result可以同時接受GET和POST傳來的值

--- index.php
+++ index.php
@@ -6,16 +6,32 @@
     <title>計算BMI</title>
 </head>
 <body>
-    <h1>計算BMI</h1>
+    <h1>計算BMI-GET</h1>
     <form action="result.php" method="get">
         <div>
             <label for="height">身高：</label>
-            <input type="numner" name="height" id="height"><br>
+            <input type="numner" name="height" id="height" step="0.1">/cm
     </div>
     <div>
 
         <label for="weight">體重：</label>
-        <input type="number" name="weight" id="weight">
+        <input type="number" name="weight" id="weight" step="0.1">/kg
+    </div>
+    <div>
+        <input type="submit" value="計算">
+        <input type="reset" value="清空/重置">
+    </div>
+    </form>
+    <h1>計算BMI-POST</h1>
+    <form action="result.php" method="post">
+        <div>
+            <label for="height">身高：</label>
+            <input type="numner" name="height" id="height" step="0.1">/cm
+    </div>
+    <div>
+
+        <label for="weight">體重：</label>
+        <input type="number" name="weight" id="weight" step="0.1">/kg
     </div>
     <div>
         <input type="submit" value="計算">
---
+++ result.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BMI結果</title>
+</head>
+<body>
+<?php
+if(isset($_GET['height'])){
+    $height=$_GET['height'];
+}else if(isset($_POST['height'])){
+    $height=$_POST['height'];
+}else{
+    echo "請使用正確管道進到此葉面!";
+    exit();
+}
+
+if(isset($_GET['weight'])){
+    $weight=$_GET['weight'];
+}else if(isset($_POST['weight'])){
+   $weight=$_POST['weight']; 
+}else{
+    echo "請使用正確管道進到此頁面!";
+    exit();
+}
+
+?>
+<h1>BMI結果</h1>
+<div>你的身高:<?=$height;?>公分</div>
+<div>你的體重:<?=$weight;?>公斤</div>
+<?php
+$h=$height/100;
+
+$bmi=round($weight/($h * $h),2);
+
+if($bmi<18.5){
+    $level="體重過輕";
+}else if($bmi<24){
+    $level="健康體位";
+}else if($bmi<27){
+    $level="過重";
+}else if($bmi<30){
+    $level="輕度肥胖";
+}else if($bmi<35){
+    $level="中度肥胖";
+}else{
+    $level="重度肥胖";
+}
+
+?>
+<div>你的BMI為:<?=$bmi;?></div>
+<div>體位判定為:<?=$level;?></div>
+<div>
+    <a href="index.php">重首頁/重新量測</a>
+</div>
+</body>
+</html>
