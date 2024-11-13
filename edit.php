d44c776e9d785ae44b118dd005aebf177f9fe100
onion <a0966550619@gmail.com>
2024-11-13 16:26:01 +0800

    完成編輯and刪除的功能

--- edit.php
+++ edit.php
@@ -5,8 +5,11 @@
 // echo "</pre>";
 
 
-$sql="insert into `member`(`acc`,`pw`,`email`,`tel`) values('{$_POST['acc']}','{$_POST['pw']}','{$_POST['email']}','{$_POST['tel']}')";
-
+$sql="UPDATE `member` SET `acc`='{$_POST['acc']}' , 
+                           `pw`='{$_POST['pw']}' ,
+                           `email`='{$_POST['email']}' ,
+                           `tel`='{$_POST['tel']}' 
+                           WHERE `id`='{$_POST['id']}'";
 
 
 $dsn="mysql:host=localhost;charset=utf8;dbname=crud";
--- success.php
+++ success.php
@@ -7,5 +7,43 @@
 </head>
 <body>
     <h1>登入成功</h1>
+
+<?php
+$dsn="mysql:host=localhost;charset=utf8;dbname=crud";
+$pdo=new PDO($dsn,'root','');
+
+$rows=$pdo->query("select * from member")->fetchAll(PDO::FETCH_ASSOC);
+
+?>
+
+<table>
+    <tr>
+        <td>id</td>
+        <td>acc</td>
+        <td>pw</td>
+        <td>email</td>
+        <td>tel</td>
+        <td>操作</td>
+    </tr>
+    <?php
+foreach($rows as  $row){
+
+?>
+    <tr>
+        <td><?=$row['id'];?></td>
+        <td><?=$row['acc'];?></td>
+        <td><?=$row['pw'];?></td>
+        <td><?=$row['email'];?></td>
+        <td><?=$row['tel'];?></td>
+        <td>
+            <a href="edit_form.php?id=<?=$row['id'];?>">編輯</a>
+            <a href="del.php?id=<?=$row['id'];?>">刪除</a>
+        </td>
+    </tr>
+    <?php
+
+}
+?>
+</table>
 </body>
 </html>
